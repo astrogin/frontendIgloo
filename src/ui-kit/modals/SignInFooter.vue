0abf851afe79d17ee64ca3ef18865cf2ce9a1779
Danila <inboxsarir@gmail.com>
2018-07-10 20:12:03 +0300

    Modals from InVision

--- src/ui-kit/modals/SignInFooter.vue
+++ src/ui-kit/modals/SignInFooter.vue
@@ -1,6 +1,17 @@
 <template>
   <span class="dialog-footer-text">
     Don't have GoAround account?
-    <a href="#" class="dialog-secondary-text">Sign up</a>
+    <a href="#" class="dialog-secondary-text" @click="openSignUp">Sign up</a>
   </span>
 </template>
+
+<script>
+  import {mapActions} from 'vuex';
+
+  export default {
+    methods: {
+      ...mapActions(['openSignUp']),
+    },
+  };
+</script>
+
--- src/ui-kit/modals/SignUpFooter.vue
+++ src/ui-kit/modals/SignUpFooter.vue
@@ -1,6 +1,16 @@
 <template>
   <span class="dialog-footer-text">
     Already have GoAround acount?
-    <a href="#" class="dialog-secondary-text">Sign in</a>
+    <a href="#" class="dialog-secondary-text" @click="openSignIn">Sign in</a>
   </span>
 </template>
+
+<script>
+  import {mapActions} from 'vuex';
+
+  export default {
+    methods: {
+      ...mapActions(['openSignIn']),
+    },
+  };
+</script>
